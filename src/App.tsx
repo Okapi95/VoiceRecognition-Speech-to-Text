451b7ad3ff4ea4c8a4a45d2ddae09d3f221e8c52
Okapi <mari.shpak.1995@gmail.com>
2023-07-10 14:04:46 +0300

    решение с помощью библиотеки react-speech-recognition (ошибки)

--- src/App.tsx
+++ src/App.tsx
@@ -1,114 +1,61 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import classes from "./App.module.css";
 import voiceOffSvg from "./icons/voiceOff.svg";
 import voiceOnSvg from "./icons/voiceOn.svg";
 import VoiceMessage from "./components/voiceMessage/voiceMessage";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
 
-declare global {
-  interface Window {
-    SpeechRecognition: any;
-    webkitSpeechRecognition: any;
-  }
-}
+// declare global {
+//   interface Window {
+//     SpeechRecognition: any;
+//     webkitSpeechRecognition: any;
+//   }
+// }
 
 function App() {
-  const SpeechRecognition =
-    window.SpeechRecognition || window.webkitSpeechRecognition;
-  const recognition = new SpeechRecognition();
-  recognition.lang = "ru-RU";
-  recognition.continuous = true;
-  recognition.interimResult = true;
+  const [currentArr, setCurrentArray] = useState<Array<string>>([]);
+  console.log("jnhbcjdrf");
+  const { transcript, browserSupportsSpeechRecognition } =
+    useSpeechRecognition();
+  if (!browserSupportsSpeechRecognition) {
+    return null;
+  }
+  const startListening = () =>
+    SpeechRecognition.startListening({
+      continuous: true,
+      language: "ru",
+    });
+  const stopListening = () => SpeechRecognition.stopListening();
 
-  // const [isListening, setIsListening] = useState(false);
-  // const [note, setNote] = useState<string>("");
-
-  // useEffect(() => {
-  //   handleListen();
-  // }, [isListening]);
-
-  // const handleListen = () => {
-  //   if (isListening) {
-  //     mic.start();
-  //     mic.onend = () => {
-  //       console.log("continue..");
-  //       mic.start();
-  //     };
-  //   } else {
-  //     mic.stop();
-  //     console.log("stop");
-  //     mic.onend = () => {
-  //       console.log("Stopped Mic on Click");
-  //     };
-  //   }
-  //   mic.onstart = () => {
-  //     console.log("Mics on");
-  //   };
-
-  //   mic.onresult = (event: any) => {
-  //     console.log(event);
-  //     console.log(Array.from(event.results));
-  //     // const transcript = event.results
-  //     //   .map((result) => result[0])
-  //     //   .map((result) => result.transcript)
-  //     //   .join("");
-  //     const transcript = "";
-  //     console.log(transcript);
-  //     setNote(() => transcript);
-  //     mic.onerror = (event: any) => {
-  //       console.log(event.error);
-  //     };
-  //   };
-  // };
-
-  recognition.onsoundend = () => console.log("прием аудио закончен");
-
-  const [wordsOnScreen, setWordsOnScreen] = useState<Array<string>>([]);
-
-  function handlerRecognitionON() {
-    recognition.start();
-    console.log("Идёт распознавание речи");
-    recognition.onstart = () => console.log("onstart");
-    setWordsOnScreen(() => ["начали распозновать"]);
-    recognition.onaudiostart = () =>
-      console.log("начинаем принимать аудио, сработал onaudiostart ");
-
-    recognition.onresult = (event: any) => {
-      console.log("сработал onresult");
-
-      const wordsArray: Array<Array<{ transcript: string }>> = Array.from(
-        event.results
-      );
-      console.log(wordsArray.map((item) => item[0].transcript));
-      // console.log("это массив wordsArray");
-
-      // const stringArr = wordsArray.map((item) => item[0].transcript);
-      // console.log(stringArr);
-
-      setWordsOnScreen(() => wordsArray.map((item) => item[0].transcript));
-    };
+  function randomInteger(min: number, max: number) {
+    let rand = min + Math.random() * (max + 1 - min);
+    return Math.floor(rand);
   }
 
-  function handlerRecognitionOFF() {
-    recognition.onsoundend = () =>
-      console.log(
-        "прием аудио закончен в handlerRecognitionOFF, сработал onsoundend"
-      );
-    console.log("stop");
-    recognition.stop();
+  const arr = transcript.split(" ");
+  if (transcript) {
+    setCurrentArray((currentArr) => [...currentArr, arr[arr.length - 1]]);
   }
+
+  console.log(currentArr);
+
+  console.log(arr);
 
   return (
     <div className={classes.app}>
       <section className={classes.appSection}>
         <div className={classes.windowOfMessage}>
-          {wordsOnScreen.map((words) => (
+          {/* {wordsOnScreen.map((words) => (
             <VoiceMessage words1={words} />
-          ))}
+          ))} */}
+          {/* {currentArr.map((word) => randomInteger(1, 100) + " " + word + " ")} */}
         </div>
         <div className={classes.buttons}>
           <button
             className={classes.buttons__microphoneButton}
-            onClick={() => handlerRecognitionON()}
+            onClick={() => startListening()}
           >
             <img
               alt="microphoneOn"
@@ -118,7 +65,7 @@ function App() {
           </button>
           <button
             className={classes.buttons__microphoneButton}
-            onClick={() => handlerRecognitionOFF()}
+            onClick={() => stopListening()}
           >
             <img
               alt="microphoneOff"
--- src/components/voiceMessage/voiceMessage.tsx
+++ src/components/voiceMessage/voiceMessage.tsx
@@ -2,16 +2,7 @@ import React from "react";
 import classes from "./voiceMessage.module.css";
 
 function VoiceMessage({ words1 }: { words1: string }) {
-  function randomInteger(min: number, max: number) {
-    let rand = min + Math.random() * (max + 1 - min);
-    return Math.floor(rand);
-  }
-
-  return (
-    <div className={classes.voiceMessage}>
-      <p>{randomInteger(1, 100)}</p> {words1}
-    </div>
-  );
+  return <div className={classes.voiceMessage}>{words1}</div>;
 }
 
 export default VoiceMessage;
